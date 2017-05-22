4a64b7a39e612d0b2ae6bd2f690c15493644086b
Roshan Basnet <roshanbasnet26@gmail.com>
2017-05-22 21:13:37 +0545

    show creditor list

---
+++ application/controllers/creditors/creditors.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roshan
+ * Date: 5/21/2017
+ * Time: 10:21 PM
+ */
+class Creditors extends CI_Controller{
+
+    public function view($page='ccreditorlist'){
+//        $data['pages'] = $page;
+////        $data['customers'] = $this->customer_model->get_customers();
+//        $this->load->view('layouts/headh');
+//        $this->load->view('layouts/sidebar');
+//        $this->load->view('customers/'.$page,$data);
+//        $this->load->view('layouts/footerh');
+        echo "testtt";
+    }
+}
+
+
+
+
+
+?>
